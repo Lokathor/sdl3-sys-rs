9b88963c728510562e01902d01db41fc26f0a095
Lokathor <zefria@gmail.com>
2024-03-25 12:23:52 -0600

    declare a minimal amount of the crate.

---
+++ src/stdinc.rs
@@ -0,0 +1,20 @@
+#![allow(non_camel_case_types)]
+
+#[inline]
+pub const fn SDL_arraysize<T, const N: usize>(_arr: &[T; N]) -> usize {
+  N
+}
+#[inline]
+pub const fn SDL_TABLESIZE<T, const N: usize>(_table: &[T; N]) -> usize {
+  N
+}
+#[inline]
+#[allow(clippy::identity_op)]
+pub const fn SDL_FOURCC(A: u8, B: u8, C: u8, D: u8) -> u32 {
+  ((A as u32) << 0)
+    | ((B as u32) << 8)
+    | ((C as u32) << 16)
+    | ((D as u32) << 24)
+}
+
+pub type SDL_bool = bool32::Bool32;
